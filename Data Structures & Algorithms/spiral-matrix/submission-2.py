d899974eeca7fa8cbdcdcccb14f57c0522c086e7
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-03-30 18:44:02 +0300

    Add: spiral-matrix - submission-2

---
+++ Data Structures & Algorithms/spiral-matrix/submission-2.py
@@ -0,0 +1,53 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        dire = [[0,1],[1,0],[0,-1],[-1,0]]
+        limits=[len(matrix[0]),len(matrix),-1,0]
+        x,y,t=0,-1,0
+        r=[]
+        while limits[0]>limits[2] and limits[1]>limits[3]:
+            d=dire[t]
+            limit=limits[t]
+            if t==0:
+                while y<limit:
+                    x+=d[0]
+                    y+=d[1]
+                    if y<limit:
+                        r.append(matrix[x][y])
+                x-=d[0]
+                y-=d[1]
+                limits[0]-=1
+            elif t==1:
+                while x<limit:
+                    print(x,y)
+                    x+=d[0]
+                    y+=d[1]
+                    if x<limit:
+                        r.append(matrix[x][y])
+                x-=d[0]
+                y-=d[1]
+                limits[1]-=1
+            elif t==2:
+                while y>limit:
+                    x+=d[0]
+                    y+=d[1]
+                    if y>limit:
+                        r.append(matrix[x][y])
+                x-=d[0]
+                y-=d[1]
+                limits[2]+=1
+            elif t==3:
+                while x>limit:
+                    x+=d[0]
+                    y+=d[1]
+                    if x>limit:
+                        r.append(matrix[x][y])
+                x-=d[0]
+                y-=d[1]
+                limits[3]+=1
+            t+=1
+            if t==4:
+                t=0
+        return r
+
+       
+        
