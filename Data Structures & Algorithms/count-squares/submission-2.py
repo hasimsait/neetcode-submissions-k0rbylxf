c788f4661a81fb659d899e5d9d50bdecdac42141
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-04-05 00:26:30 +0300

    Add: count-squares - submission-2

---
+++ Data Structures & Algorithms/count-squares/submission-2.py
@@ -0,0 +1,28 @@
+class CountSquares:
+
+    def __init__(self):
+        self.pointsByX={}
+        self.pointsByY={}
+        self.points = []
+
+    def add(self, point: List[int]) -> None:
+        if point[0] not in self.pointsByX:
+            self.pointsByX[point[0]]=[]
+        self.pointsByX[point[0]].append(point[1])
+        if point[1] not in self.pointsByY:
+            self.pointsByY[point[1]]=[]
+        self.pointsByY[point[1]].append(point[0])
+        self.points.append(point)
+
+    def count(self, p: List[int]) -> int:
+        res=0
+        for p2 in self.points:
+            x2=p2[0]
+            y2=p2[1]
+            if x2!=p[0] and y2!=p[1]:
+                #diagonals so we dont count the same sq thrice
+                a=[x for x in self.pointsByY[y2] if x==p[0]]
+                b=[y for y in self.pointsByX[x2] if y==p[1]]
+                res+=len(a)*len(b)
+        return res
+        
