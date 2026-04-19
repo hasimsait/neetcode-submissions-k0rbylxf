3c426bdf95bed3a0913fad52cd4e98f230482895
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-04-19 03:37:18 +0300

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        words=set()
+        lenmin=999
+        lenmax=0
+        for word in wordDict:
+            words.add(word)
+            lenmin = min(lenmin,len(word))
+            lenmax = max(lenmax,len(word))
+        mem = {}
+        def c(i):
+            if len(s)-i in mem:
+                return mem[len(s)-i]
+            if len(s)<=i or s[i:] in words:
+                mem[len(s)-i] = True
+                return True
+            for j in range(lenmin,lenmax+1)[::-1]:
+                p = s[i:i+j]
+                if p in words:
+                    if c(i+j):
+                        mem[len(s)-i]=True
+                        return True
+            mem[len(s)-i]=False
+            return False
+        return c(0)
+
+            
