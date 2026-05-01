0d117933c738f63ad996be82f7b550f030f124cc
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-05-02 01:05:47 +0300

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.py
@@ -0,0 +1,57 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wlen = len(wordList[0])
+        class Word:
+            def __init__(self,w):
+                self.w = w
+                self.neigh = set()
+            def addNeigh(self,w):
+                word = wordList[w]
+                sw=wordList[self.w]
+                a=0
+                for c in range(wlen):
+                    if word[c]!=sw[c]:
+                        a+=1
+                    if a>1:
+                        return
+                if a == 1:
+                    self.neigh.add(w)
+        wordList.append(beginWord)
+        words = {}
+        words[len(wordList)-1] = Word(len(wordList)-1)
+        for a in range(len(wordList)-1):
+            words[len(wordList)-1].addNeigh(a)
+            words[a]=Word(a)
+            for b in range(len(wordList)-1):
+                words[a].addNeigh(b)
+        queue = [(1,len(wordList)-1)]
+        visited = [0 for x in range(len(wordList))]
+        target = -1
+        for i in range(len(wordList)):
+            if wordList[i]==endWord:
+                target = i
+        if target == -1:
+            #unreachable since node doesn't exist
+            return 0
+        while len(queue)!=0:
+            cu=queue.pop(0)
+            cost = cu[0]
+            wordIndex = cu[1]
+            if visited[wordIndex]:
+                continue
+            if wordIndex == target:
+                return cost
+            visited[wordIndex] =1
+            for x in words[wordIndex].neigh:
+                if visited[x] == 0:
+                    queue.append((cost+1,x))
+        #unreachable bc intermediate state is missing
+        return 0
+            
+
+
+
+            
+                
+
+        
