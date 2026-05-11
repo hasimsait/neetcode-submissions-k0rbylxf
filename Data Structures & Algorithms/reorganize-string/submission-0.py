04487b44f9181964d745cf2ef2c21e65551b469d
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-05-11 19:31:21 +0300

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        c= defaultdict(int)
+        for x in s:
+            c[x]+=1
+        a=[]
+        for ch in c:
+            heapq.heappush(a,(-1*c[ch],ch))
+        n=len(s)
+        r=[' ']
+        while len(r)<=n:
+            c=r[-1]
+            t=heapq.heappop(a)
+            if t[1]==c:
+                if len(a)>0:
+                    tt=heapq.heappop(a)
+                    heapq.heappush(a,t)
+                    t=tt
+                else:
+                    return ""
+            r.append(t[1])
+            if t[0]<-1:
+                heapq.heappush(a,(t[0]+1,t[1]))
+        return "".join(r[1:])
+
+
