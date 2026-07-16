0d86cae1153dcd6b62b39b9d5a7e497b54ffe7a4
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-07-16 23:35:13 +0300

    Add: capacity-to-ship-packages-within-d-days - submission-0

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        def works(c):
+            s,cc=1,c
+            for w in weights:
+                if cc-w<0:
+                    s+=1
+                    if s>days:
+                        return False
+                    cc=c
+                cc-=w
+            return True
+        l,r = max(weights),sum(weights) #must be able to ship the largest load
+        n=r
+        while l<=r:
+            c=(l+r)//2
+            if works(c):
+                n=min(n,c)
+                r=c-1
+            else:
+                l=c+1
+        return n
