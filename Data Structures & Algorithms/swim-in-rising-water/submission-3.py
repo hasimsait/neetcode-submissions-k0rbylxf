c81cce0cada8491e997ead9f6f73a7d6ab7c9ff1
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-07-28 01:48:35 +0300

    Add: swim-in-rising-water - submission-3

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        di = [(1,0),(0,1),(-1,0),(0,-1)]
+        mem = [[len(grid)**2]*len(grid) for g in grid]
+        def b(t,i,j):
+            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]):
+                return False
+            if grid[i][j]>t:
+                t=grid[i][j]
+            if mem[i][j]>t:
+                for d in di:
+                    inew = d[0]+i
+                    jnew = d[1]+j
+                    heapq.heappush(s,(t,inew,jnew))
+                mem[i][j] = t
+                return True
+            return False
+        s=[]
+        heapq.heappush(s,(max(0,grid[0][0]),0,0))
+        while s and mem[-1][-1]==len(grid)**2:
+            t,i,j=heapq.heappop(s)
+            b(t,i,j)
+        return mem[-1][-1]
