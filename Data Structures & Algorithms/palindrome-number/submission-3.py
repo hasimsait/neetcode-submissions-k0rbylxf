f6c4af5f39c0344a429958699a07231bf889ecd6
Haşim Sait Göktan <33639852+hasimsait@users.noreply.github.com>
2026-08-23 18:01:03 +0300

    Add: palindrome-number - submission-3

---
+++ Data Structures & Algorithms/palindrome-number/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        def getLog10(x):
+            if x<10:
+                return 0
+            if x!=0 and x/10>1:
+                return getLog10(x/10)+1
+            return -1
+        if x<0 or (x!=0 and x%10==0):
+            return False
+        if x==0:
+            return True
+        i,j=getLog10(x),1
+        while i>=j:
+            if (x//(10**i))%(10)!=(x%10**j)//10**(j-1):
+                return False
+            i-=1
+            j+=1
+        return True
